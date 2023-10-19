0434d66513dc536b7b6294db8aa543b4d58f4a86
OnlyHalfEvil333 <95574693+OnlyHalfEvil333@users.noreply.github.com>
2023-10-19 23:35:19 +0600

    init

---
+++ src/main/java/com/example/electronicsstore/ElectronicsstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/electronicsstore/ElectronicsstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
